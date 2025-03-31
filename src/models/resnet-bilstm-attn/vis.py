3ec2a13563d14234dd3e04ea87bf6c9dbacda280
Daniel Fischer <dfischer@qunevo.com>
2025-03-31 17:32:54 +0200

    test

--- src/models/resnet-bilstm-attn/vis.py
+++ src/models/resnet-bilstm-attn/vis.py
@@ -15,4 +15,4 @@ model_graph = torchview.draw_graph(
 model_graph.visual_graph.graph_attr.update(fontname="Times New Roman", fontsize="14")
 model_graph.visual_graph.node_attr.update(fontname="Times New Roman", fontsize="14")
 model_graph.visual_graph.edge_attr.update(fontname="Times New Roman", fontsize="14")
-model_graph.visual_graph
+model_graph.visual_graphgit config user.email "new_email@example.com"
